179528bf115422b111f453f22ecd591d3ab8d504
Prashant Mishra <mishraprashant916@gmail.com>
2023-09-08 00:08:13 +0530

    UPDATED CODE

--- src/Project/ConnectionProvider.java
+++ src/Project/ConnectionProvider.java
@@ -17,10 +17,10 @@ public class ConnectionProvider {
         {
             Class.forName("com.mysql.cj.jdbc.Driver");
            //Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/lms","root","prashant@24051998");
-           Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/lms?autoReconnect=true&useSSL=false","root","prashant@24051998");
+           Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/lms?autoReconnect=true&useSSL=false","root","prashant@2405");
             return con;
         }
-        catch(Exception e)
+        catch(ClassNotFoundException | SQLException e)
         {
             System.out.println(e);
             return null;
